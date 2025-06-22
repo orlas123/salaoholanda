83cb45623b197bbce4fa623fd86394d28ea97907
Orlacomer <162639930+orlas123@users.noreply.github.com>
2025-06-22 16:03:19 +0300

    Add files via upload

---
+++ index (1).js
@@ -0,0 +1,28 @@
+const cart = document.getElementById('cart');
+let cartContagen = 0;
+button. addEventListener ('click' , () => {
+    const nome = document.getElementById('nome').value;
+    const numero = document.getElementById('numero').value;
+    if (nome && numero) {
+
+        
+    } else{
+        alert('escrivi na tudu campos .');
+    }
+
+});
+
+<button onclick="documento.GETELEMENTBYID ('demo').innnerhtml=DATE()">clica aqui</button>
+button.addEventListener('click', () =>{
+    if (button = onclick Event) {
+        ultimo-pedido.document.getElementById(pedido, )
+    }
+})
+
+
+
+
+
+b
+}
+construacao de um aplicativo vai ser mas facil do que penca vamos fazzer mas facil para incerir as imagem asim que concluimos vamos te informar  
